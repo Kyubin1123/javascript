6326a765c1d5a7ecae629d3b716b78e90e2bb19a
kyubin1123 <choiseung-won@choeseung-won-ui-MacBookPro.local>
2024-09-02 18:16:32 +0900

    js 3회차 과제완성

--- 3회차/join.js
+++ 3회차/join.js
@@ -27,6 +27,15 @@ form.addEventListener("submit", function(event){
 
     document.body.innerHTML = ""
     document.write(`<p>${userId}님 환영합니다.</p>`)
+    document.write(
+        `<p>
+            회원 가입 시 입력하신 내역은 다음과 같습니다.<br>
+            아이디 : ${userId}<br>
+            이름 : ${username}<br>
+            전화번호 : ${userPhone}<br>
+            원하는 직무 : ${userPosition}
+        </p>`
+    )
 })
 //제출된 입력 값들을 참조한다
 
